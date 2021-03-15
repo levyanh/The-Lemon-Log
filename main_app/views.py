7e3d96416027d7594bc1262d691d50ab757bed76
levyanh <levyanh1@gmail.com>
2021-03-15 12:06:08 -0700

    setup profile page

--- main_app/views.py
+++ main_app/views.py
@@ -4,12 +4,20 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
 # Add home view:
+@login_required
 def home(request):
     return render(request, 'home.html')
+
 # Add about view:
 @login_required
 def about(request):
     return render(request,'about.html')
+
+# Add profile view:
+@login_required
+def profile(request):
+    return render(request,'profile.html')
+
 # Add signup view:
 def signup(request):
   error_message = ''
@@ -22,7 +30,7 @@ def signup(request):
       user = form.save()
       # This is how we log a user in via code
       login(request, user)
-      return redirect('index')
+      return redirect('home')
     else:
       error_message = 'Invalid sign up - try again'
   # A GET or a bad POST request, so render signup.html with an empty form
