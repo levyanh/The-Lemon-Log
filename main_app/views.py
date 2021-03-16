d63ba700e0cb1bc76489cff189472134eb8844dc
levyanh <levyanh1@gmail.com>
2021-03-16 09:58:54 -0700

    update signup form with name, email

--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm
+
 
 # Add home view:
-@login_required
 def home(request):
     return render(request, 'home.html')
 
 # Add about view:
-@login_required
 def about(request):
     return render(request,'about.html')
 
@@ -18,22 +18,32 @@ def about(request):
 def profile(request):
     return render(request,'profile.html')
 
+# # Add signup view:
+# def signup(request):
+#   error_message = ''
+#   if request.method == 'POST':
+#     form = UserCreationForm(request.POST)
+#     if form.is_valid():
+#       user = form.save()
+#       login(request, user)
+#       return redirect('home')
+#     else:
+#       error_message = 'Invalid sign up - try again'
+#   form = UserCreationForm()
+#   context = {'form': form, 'error_message': error_message}
+#   return render(request, 'registration/signup.html', context)
+
 # Add signup view:
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
-    form = UserCreationForm(request.POST)
+    form = SignUpForm(request.POST)
     if form.is_valid():
-      # This will add the user to the database
       user = form.save()
-      # This is how we log a user in via code
       login(request, user)
-      return redirect('home')
+      return redirect('profile')
     else:
       error_message = 'Invalid sign up - try again'
-  # A GET or a bad POST request, so render signup.html with an empty form
-  form = UserCreationForm()
+  form = SignUpForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
