8f5d45137bb3471a3dbd3779b5d158c9669b5e13
levyanh <levyanh1@gmail.com>
2021-03-16 11:31:15 -0700

    add profile and review models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+
+
+
+# Add Review model:
+class Review(models.Model):
+    title = models.CharField(max_length=255)
+    rating = models.FloatField(default=0)
+    text = models.TextField(max_length=2000)
+    image = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- main_app/views.py
+++ main_app/views.py
@@ -18,21 +18,6 @@ def about(request):
 def profile(request):
     return render(request,'profile.html')
 
-# # Add signup view:
-# def signup(request):
-#   error_message = ''
-#   if request.method == 'POST':
-#     form = UserCreationForm(request.POST)
-#     if form.is_valid():
-#       user = form.save()
-#       login(request, user)
-#       return redirect('home')
-#     else:
-#       error_message = 'Invalid sign up - try again'
-#   form = UserCreationForm()
-#   context = {'form': form, 'error_message': error_message}
-#   return render(request, 'registration/signup.html', context)
-
 # Add signup view:
 def signup(request):
   error_message = ''
