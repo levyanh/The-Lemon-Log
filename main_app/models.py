7cccd7de99745b213613c4aff0ceed38d1d9de3b
levyanh <levyanh1@gmail.com>
2021-03-16 13:39:45 -0700

    Add username, join date and default avarta in profile page

--- main_app/models.py
+++ main_app/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
-
-
-
+# Add Profile model:
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avarta = models.ImageField()
 
 # Add Review model:
 class Review(models.Model):
     title = models.CharField(max_length=255)
     rating = models.FloatField(default=0)
     text = models.TextField(max_length=2000)
-    image = models.IntegerField()
+    image = models.ImageField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
