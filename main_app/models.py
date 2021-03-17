25e47b8cb1609f11fde1f0a1f60411d09145aab1
ABHI <am@MacBook-Pro.local>
2021-03-17 09:01:14 -0700

    changes the name of a model to Profile

--- main_app/models.py
+++ main_app/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class UserProfileInfo(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     # additional attributes
-    profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
+    profile_pic = models.ImageField(default='default.jpg',upload_to='profile_pics')
 
     def __str__(self):
         return self.user.username
