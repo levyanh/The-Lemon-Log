96935619769d8cfb8129ab53eebbe3f1f0765545
ABHI <am@MacBook-Pro.local>
2021-03-19 07:11:27 -0700

    fixes error while signup

--- main_app/views.py
+++ main_app/views.py
@@ -92,21 +92,15 @@ def profile(request):
 def signup(request):
   registered = False
   if request.method == 'POST':
-    # Get info from "both" forms
-    # It appears as one form to the user on the .html page
     user_form = UserForm(data=request.POST)
     profile_form = UserProfileInfoForm(data=request.POST)
-
-    # Check to see both forms are valid
-    if user_form.is_valid() and profile_form.is_valid():
-          
+    if user_form.is_valid() and profile_form.is_valid():   
       # Save User Form to Database
       user = user_form.save()
       # Hash the password
       user.set_password(user.password)
       # Update with Hashed password
       user.save()
-
       # Now we deal with the extra info!
       # Can't commit yet because we still need to manipulate
       profile = profile_form.save(commit=False)
@@ -118,21 +112,16 @@ def signup(request):
         print('found it')
         # If yes, then grab it from the POST form reply
         profile.profile_pic = request.FILES['profile_pic']
-        # Now save model
-        profile.save()
         # Registration Successful!
         registered = True
       else:
-        # One of the forms was invalid if this else gets called.
         print(user_form.errors,profile_form.errors)
+      profile.save()
       login(request,user)
       return redirect('profile')
   else:
-      # Was not an HTTP post so we just render the forms as blank.
     user_form = UserForm()
     profile_form = UserProfileInfoForm()
-  # This is the render and context dictionary to feed
-  # back to the registration.html file page.
   context = {'user_form': user_form, 'profile_form':profile_form, 'registered':registered}
   return render(request, 'registration/signup.html', context)
 
