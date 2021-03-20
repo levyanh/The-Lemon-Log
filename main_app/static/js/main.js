9431bd05d2f9dd4c78aeeb10e0dab0d1c012377d
ABHI <am@MacBook-Pro.local>
2021-03-20 16:14:48 -0700

    fix some bugs in navbar

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -1,12 +1,12 @@
-// const nav = document.getElementById('nav');
-//   window.onscroll = function(){
-//     if (window.pageXOffset >100) {
-//       nav.style.background = "black";
-//     } 
-//     else {
-//       nav.style.background = "transparent"
-//     }
-//   }
+const nav = document.getElementById('nav');
+  window.onscroll = function(){
+    if (window.pageXOffset >100) {
+      nav.style.background = "black";
+    } 
+    else {
+      nav.style.background = "transparent"
+    }
+  }
 
 const slider = document.querySelector(".slider");
     M.Slider.init(slider, {
