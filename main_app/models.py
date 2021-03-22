c7503f1d833a759d9f8a7731823fc6c1e0880032
ABHI <am@MacBook-Pro.local>
2021-03-22 04:40:12 -0700

    updates models

--- main_app/models.py
+++ main_app/models.py
@@ -46,7 +46,6 @@ class Comment(models.Model):
 class Photo(models.Model):
     url = models.CharField(max_length=200)
     review = models.ForeignKey(Review, on_delete=models.CASCADE)
-    # profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Photo for review_id: {self.review_id} @{self.url}"
