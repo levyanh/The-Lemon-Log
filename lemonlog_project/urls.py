f41fa86eba205f8df8a16e3c4d7184ca5eba8b4a
ABHI <am@MacBook-Pro.local>
2021-03-21 17:13:29 -0700

    adds changes in setting.py

--- lemonlog_project/urls.py
+++ lemonlog_project/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_app.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-]+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
-# if settings.DEBUG:
-#     urlpatterns 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
